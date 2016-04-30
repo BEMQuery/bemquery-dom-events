606470267135feea627c74c9f4e0423cee1a509e
Comandeer <vepomoc@gmail.com>
2016-04-30 02:53:21 +0200

    [ci skip] Updated config for mockr.

--- config/mockr/default.js
+++ config/mockr/default.js
@@ -1,5 +1,10 @@
 'use strict';
 
-module.exports = {
+const cwd = process.cwd();
+const config = {};
 
+config[ `${cwd}/tests/listeners.js` ] = config[ `${cwd}/tests/module.js` ] = {
+		'bemquery-core': `${cwd}/tests/support/mocks/bemquery-core.js`
 };
+
+module.exports = config;
